846791205ac3c8a1d29cf41fcdc552838a6e4774
Triaci <triacis15937@gmail.com>
2022-05-11 19:34:34 -0300

    Initial commit

---
+++ src/main/java/br/fag/edu/locacao/model/Base_Model.java
@@ -0,0 +1,31 @@
+package br.fag.edu.locacao.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+public class Base_Model {
+    @Column
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dtCreate;
+
+    @Column
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dtUpdate;
+
+    @Column
+    private long version;
+
+    @PrePersist
+    private void prePersist(){
+        version = 0;
+        dtCreate = new Date();
+        dtUpdate = new Date();
+    }
+
+    @PreUpdate
+    private void preUpdate(){
+        version ++;
+        dtUpdate = new Date();
+    }
+
+}
