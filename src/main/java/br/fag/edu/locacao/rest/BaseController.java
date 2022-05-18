eb200dcc178e8a8257e35b8e9a445f98a871d1af
Lucas Condé <78701182+Condelamar@users.noreply.github.com>
2022-05-18 20:07:40 -0300

    Propriedades

--- src/main/java/br/fag/edu/locacao/rest/BaseController.java
+++ src/main/java/br/fag/edu/locacao/rest/BaseController.java
@@ -2,6 +2,7 @@ package br.fag.edu.locacao.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -15,6 +16,9 @@ public abstract class BaseController<T> {
     public  abstract T find(@RequestParam String id);
 
     @PostMapping("/insert")
-    public abstract void insert(T insertObjeto);
+    public abstract void insert(@RequestBody T insertObjeto);
+
+    @PostMapping("/update")
+    public abstract void update(@RequestBody T updateObjeto);
 
 }
